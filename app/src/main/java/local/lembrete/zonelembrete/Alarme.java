0ac1d13710cf1539df54766e7fff4cc9cd65a733
Rui Neves Carvalho <astralenigma@hotmail.com>
2016-12-21 18:43:40 +0000

    Adicionadas variáveis data ao Alarme.

--- app/src/main/java/local/lembrete/zonelembrete/Alarme.java
+++ app/src/main/java/local/lembrete/zonelembrete/Alarme.java
@@ -1,15 +1,24 @@
 package local.lembrete.zonelembrete;
 
+import java.util.Date;
+
 /**
- * Created by Rui on 09-Dec-16.
+ * Alarme é o objecto usado para guardar a informação do Alarme.
  */
 
 class Alarme {
     private String nome;
+    private Date dataInicio;
+    private Date dataFim;
 
     //Para testes.
-     Alarme(String nome){
+    Alarme(String nome){
         this.nome=nome;
+    }
+    Alarme(String nome, Date dataInicio, Date dataFim){
+        this.nome=nome;
+        this.dataInicio=dataInicio;
+        this.dataFim=dataFim;
     }
     @Override
     public String toString() {
