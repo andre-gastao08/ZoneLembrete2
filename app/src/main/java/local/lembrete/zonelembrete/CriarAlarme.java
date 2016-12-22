5d30b7b33973e6734e0aa887d28e4461469e86d4
Rui Neves Carvalho <astralenigma@hotmail.com>
2016-12-22 12:53:32 +0000

    Adicionada imagem à interface do alarme. E outros retoques extra.

--- app/src/main/java/local/lembrete/zonelembrete/CriarAlarme.java
+++ app/src/main/java/local/lembrete/zonelembrete/CriarAlarme.java
@@ -1,14 +1,23 @@
 package local.lembrete.zonelembrete;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class CriarAlarme extends AppCompatActivity {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView cameraView;
+    EditText nome;
+    EditText datainicio;
+    EditText datafim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +25,10 @@ public class CriarAlarme extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final EditText nome = (EditText) findViewById(R.id.nome);
-        final EditText datainicio = (EditText) findViewById(R.id.dataInicio);
-        EditText datafim = (EditText) findViewById(R.id.datafim);
-
+        nome = (EditText) findViewById(R.id.nome);
+        datainicio = (EditText) findViewById(R.id.dataInicio);
+        datafim = (EditText) findViewById(R.id.dataFim);
+        cameraView = (ImageView) findViewById(R.id.imageCamera);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,4 +43,21 @@ public class CriarAlarme extends AppCompatActivity {
         });
     }
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            cameraView.setImageBitmap(imageBitmap);
+        }
+
+    }
 }
