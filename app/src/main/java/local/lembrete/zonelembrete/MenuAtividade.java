e75d2429a849c574c15f4a110a1b950e536ff545
Rui Neves Carvalho <astralenigma@hotmail.com>
2016-12-22 23:42:34 +0000

    Que se lixem os menus.

--- app/src/main/java/local/lembrete/zonelembrete/MenuAtividade.java
+++ app/src/main/java/local/lembrete/zonelembrete/MenuAtividade.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 
@@ -32,7 +30,7 @@ public class MenuAtividade extends AppCompatActivity {
             public void onClick(View view) {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
-                criarAlarme(view);
+                //criarAlarme(view);
             }
         });
 
@@ -46,28 +44,6 @@ public class MenuAtividade extends AppCompatActivity {
         listView.setAdapter(adapter);
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_menu_atividade, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void criarAlarme(View view) {
